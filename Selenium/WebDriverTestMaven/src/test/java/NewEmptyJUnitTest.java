2608944ae5be70e7bf6db62880f07d8e89f80042
alekskw95 <32495209+alekskw95@users.noreply.github.com>
2017-12-11 22:34:11 +0100

    Add files via upload

---
+++ Selenium/WebDriverTestMaven/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class NewEmptyJUnitTest {
+
+  @Test
+  public void hello() {
+    /*WebDriver driver  = new FirefoxDriver();
+    //WebDriver driver  = new ChromeDriver();
+      driver.get("https:\\seleniumeasy.com\\test");
+      WebElement acceptAnswerLink = driver.findElement(By.id("vote-accepted-13214"));
+      acceptAnswerLink.click();  
+        driver.quit();*/
+    
+    /*System.setProperty("webdriver.chrome.driver", "path of the exe file\\chromedriver.exe");
+ 
+    // Initialize browser
+    WebDriver driver2=new ChromeDriver();
+ 
+    // Open facebook
+    driver2.get("http://www.facebook.com");
+ 
+    // Maximize browser
+ 
+    driver2.manage().window().maximize();*/
+  }
+}
